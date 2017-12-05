31291e37cf1a3f053bf6175198690a48a619059d
Bence Kiss <official.bencekiss@gmail.com>
2017-12-05 08:03:05 -0800

    Implement binary and view logic

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+function getLastNthDigit(matrix, n) {
+    return (Math.floor(matrix / 2 ** n)) % 2;
+}
+
 window.onload = function() {
 
+    var matrix = 49200;
     var display = document.querySelector(".display");
 
-    for(var i = 1;i < 21;i++) {
-        for(var j = 1;j < 11;j++) {
-            display.insertAdjacentHTML('beforeend',
-            '<div class="pixel" id=' + i + '-' + j + '></div>');
-        }
+    for (var i = 0; i < 200; i++) {
+            var status;
+            if (getLastNthDigit(matrix, i)) {
+                status = "pixel-active";
+            } else {
+                status = "pixel-inactive";
+            }
+            display.insertAdjacentHTML(
+                'beforeend',
+                '<div class="' + status + '"></div>'
+            );
     }
-
 }
