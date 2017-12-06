633e4f3107c3f645bb6b70b37442924f91b2a953
gabripeti@gmail.com <gabripeti@gmail.com>
2017-12-06 11:29:21 +0100

    Add bigInt

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function getLastNthDigit(matrix, n) {
-    return (Math.floor(matrix / 2 ** n)) % 2;
+function getLastNthDigitOfBigInt(matrix, n) {
+    var divider = bigInt(2).pow(n);
+    return matrix.divide(divider).mod(2).value;
 }
 
 window.onload = function() {
-
-    var matrix = 49200;
+    var matrix = bigInt(49200);
     var display = document.querySelector(".display");
 
     for (var i = 0; i < 200; i++) {
-            var status;
-            if (getLastNthDigit(matrix, i)) {
-                status = "pixel-active";
-            } else {
-                status = "pixel-inactive";
-            }
-            display.insertAdjacentHTML(
-                'beforeend',
-                '<div class="' + status + '"></div>'
-            );
+        var status;
+        if (getLastNthDigitOfBigInt(matrix, i)) {
+            status = "pixel-active";
+        } else {
+            status = "pixel-inactive";
+        }
+        display.insertAdjacentHTML(
+            'beforeend',
+            '<div class="' + status + '"></div>'
+        );
     }
-}
+};
