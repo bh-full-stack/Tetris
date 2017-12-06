20d2bab7aaa3d19985534f753c7b22410c9674b6
Mob Machine <mob@stylersonline.com>
2017-12-06 15:18:58 +0100

    Detect both walls

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ function getLastNthDigitOfBigInt(matrix, n) {
 }
 
 window.onload = function() {
-    var matrix = bigInt(49200);
-    var display = document.querySelector(".display");
     renderDisplay();
 
+    var leftWall = bigInt(0);
+    var rightWall = bigInt(0);
+    var matrix = bigInt(49200);
     var pixelList = document.querySelectorAll(".pixel");
+    
     refreshDisplay();
+
+    for (var i = 0; i < 191; i += 10) {
+        leftWall = leftWall.add(bigInt(2).pow(i));
+    }
+
+    for (var i = 9; i < 200; i += 10) {
+        rightWall = rightWall.add(bigInt(2).pow(i));
+    }
 
     function renderDisplay() {
         for (var i = 0; i < 200; i++) {
-            display.insertAdjacentHTML("beforeend", "<div class='pixel'></div>");
+            document
+                .querySelector(".display")
+                .insertAdjacentHTML("beforeend", "<div class='pixel'></div>");
         }
     }
 
@@ -30,15 +42,17 @@ window.onload = function() {
     document.onkeydown = function(event) {
         switch (event.key) {
             case "ArrowLeft":
-                if (!pixelList[0].classList.contains("active")) {
+                if (leftWall.and(matrix) == 0) {
                     matrix = matrix.divide(2);
                 }
                 break;
             case "ArrowRight":
-                if (!pixelList[9].classList.contains("active")) {
+                if (rightWall.and(matrix) == 0) {
                     matrix = matrix.multiply(2);
                 }
                 break;
+            case "ArrowDown":
+                matrix = matrix.multiply(2 ** 10);
         }
         refreshDisplay();
     }
