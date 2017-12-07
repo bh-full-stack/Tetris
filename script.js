7d88c11bf96ce74bb483188911f147ebce2c6baf
Mob Machine <mob@stylersonline.com>
2017-12-07 11:48:19 +0100

    Add new elements and randomizer

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 window.onload = function() {
 
+    var world = bigInt(0);
     var leftBorder = bigInt(0);
     var rightBorder = bigInt(0);
     var bottomBorder = bigInt(0);
-    var element = bigInt(49200);
-    var world = bigInt(0);
+    var element = bigInt(0);
+    var elements = [
+        bigInt(49200),
+        bigInt(120),
+        bigInt(57352),
+        bigInt(57376),
+        bigInt(24624),
+        bigInt(49176),
+        bigInt(57360)
+    ];
 
-    for (var i = 0; i < 191; i += 10) {
-        leftBorder = leftBorder.add(bigInt(2).pow(i));
-    }
-    for (i = 9; i < 200; i += 10) {
-        rightBorder = rightBorder.add(bigInt(2).pow(i));
-    }
-    for (i = 191; i < 200; i++) {
-        bottomBorder = bottomBorder.add(bigInt(2).pow(i));
+    function getRandomElement() {
+        element = elements [Math.floor(Math.random()*6)];
     }
 
-    function descend() {
+    function createBorders() {
+        for (var i = 0; i < 191; i += 10) {
+            leftBorder = leftBorder.add(bigInt(2).pow(i));
+        }
+        for (i = 9; i < 200; i += 10) {
+            rightBorder = rightBorder.add(bigInt(2).pow(i));
+        }
+        for (i = 191; i < 200; i++) {
+            bottomBorder = bottomBorder.add(bigInt(2).pow(i));
+        }
+    }
+
+    function descendElement() {
         if (bottomBorder.and(element) == 0) {
             element = element.multiply(2 ** 10);
         } else {
             world = world.or(element);
-            element = bigInt(49200);
+            getRandomElement();
         }
         refreshDisplay(element, world);
     }
 
+    createBorders();
     renderDisplay();
+    getRandomElement();
     refreshDisplay(element, world);
-    setInterval(descend, 200);
+    setInterval(descendElement, 200);
 
     document.onkeydown = function(event) {
         switch (event.key) {
